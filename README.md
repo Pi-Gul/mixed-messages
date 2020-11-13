202708eb59eb150b086807bffdb4026bdc6ee6f6
Pi-Gul <photon-drive@protonmail.com>
2020-11-13 21:49:30 +0100

    Modify README.md; add arrays with sentence fragments and assembling function

--- README.md
+++ README.md
@@ -7,8 +7,3 @@ MM picks random elements from predefined arrays of phrases and inserts them into
 ## What is the main theme of the messages?
 MM generates messages styled as predictions of the state of the world in 2120.
 
-### Features to implement:
-- [ ] Create an array with sentence templates.
-- [ ] Create an array with phrases.
-- [ ] Create a function that picks a random phrase and inserts it into a template.
-- [ ] Log the result to the console.
--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-console.log('TEST');
+const prefaceArr = [
+'In the year 2020',
+'In the not too distant future',
+'A hundred years from now'
+];
+const subjArr = [
+'humans',
+'giraffes',
+'elephants',
+'hamsters',
+'cephalopods'
+];
+const verbArr = [
+'build',
+'destroy',
+'discover',
+'invent',
+'rebuild',
+'forget'
+];
+const objArr = [
+'a base',
+'a research station',
+'a train station',
+'a sky-scraper',
+'a circus',
+'a cell-phone factory',
+'an ice-cream stand'
+];
+const placeArr = [
+'on Earth',
+'on the Moon',
+'on Mars',
+'in the Trappist system',
+'in another galaxy'
+];
+
+function assembleSentence() {
+	let message = '';
+	let index = Math.floor(Math.random() * prefaceArr.length);
+	message += ' '+prefaceArr[index];
+	index = Math.floor(Math.random() * subjArr.length);
+	message += ' '+subjArr[index];
+	index = Math.floor(Math.random() * verbArr.length);
+	message += ' will '+verbArr[index];
+	index = Math.floor(Math.random() * objArr.length);
+	message += ' '+objArr[index];
+	index = Math.floor(Math.random() * placeArr.length);
+	message += ' '+placeArr[index];	
+	return message;
+};
+
+console.log(assembleSentence());
